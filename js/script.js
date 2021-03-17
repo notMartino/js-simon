313e7d7a82b38c0b086b51e928fd984ea1effbb7
Martino Scalvini <martinoscalvinitobia@gmail.com>
2021-03-17 15:22:53 +0100

    Esercizio completo: JS funzionante

--- js/script.js
+++ js/script.js
@@ -22,12 +22,60 @@ function alertMemory() {
     alert('You have 30s then you rewrite these numbers: ' + rndNumbers);
 }
 
+// Funzione inserimento numeri
+function insertNumbers(){
+    while (inputNumbers.length < 5) {
+        var numInput = parseInt(prompt('Inserisci il numero: '));
+        if (!isNaN(numInput) && numInput > 0 && numInput <= 100) {
+            if (inputNumbers.includes(numInput)) {
+                console.log('Errore: numero già inserito!');
+                continue;
+            }
+            inputNumbers.push(numInput);
+            continue;
+        }
+        console.log('Errore: inserisci un numero da 1 a 100!');
+    }
+    var points = calcPoints();
+    // console.log('Random numbers: ' + rndNumbers);
+    console.log('Your numbers: ' + inputNumbers);
 
+    if(points > 0){
+        console.log('Correct numbers: ' + correctNumbers);
+    }
+    else{
+        console.log('Correct numbers: none');
+    }
+
+    console.log('Total points: ' + points);
+    
+
+}
+
+// Funzione calcolo punti e numeri vincenti
+function calcPoints() {
+    var punti = 0;
+    for (let i = 0; i < inputNumbers.length; i++) {
+        for (let j = 0; j < rndNumbers.length; j++) {
+            if (inputNumbers[i] == rndNumbers[j]) {
+                punti++;
+                correctNumbers.push(inputNumbers[i]);
+            }
+        }
+    }
+    return punti;
+}
+
+function init(){
+    alertMemory();
+    setTimeout(insertNumbers, 3000);  
+}
 
 // -------------------- Normal code -----------------------
 var rndNumbers = [];
 var inputNumbers = [];
 var correctNumbers = [];
 
-var punteggio = init();
-console.log('');
+init();
+
+console.log('Random numbers: ' + rndNumbers);
